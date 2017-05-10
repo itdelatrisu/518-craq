05ba87a4e8be21533562de9eb11f188ff6ae957f
Jeffrey Han <itdelatrisu@gmail.com>
2017-05-10 19:17:39 -0400

    Update installation script

--- emulab_install.sh
+++ emulab_install.sh
@@ -5,13 +5,11 @@
 # Emulab OSID: UBUNTU14-64-STD
 # ------------------------------------------------
 
-# install add-apt-repository
+# install packages
 sudo apt-get -y install software-properties-common python-software-properties
-
-# install openjdk-8 and maven
 sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt-get update
-sudo apt-get -y install openjdk-8-jdk maven
+sudo apt-get -y install openjdk-8-jdk maven screen
 
 # configure java binaries
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
