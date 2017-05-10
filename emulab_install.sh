52372ad0769cf8567fb234a77ea9677889855c52
Jeffrey Han <itdelatrisu@gmail.com>
2017-05-10 17:41:58 -0400

    chmod +x

--- emulab_install.sh
+++ emulab_install.sh
@@ -16,9 +16,3 @@ sudo apt-get -y install openjdk-8-jdk maven
 # configure java binaries
 sudo update-alternatives --config java
 sudo update-alternatives --config javac
-
-# set up project
-cd ~
-git clone https://github.com/itdelatrisu/518-craq.git
-cd 518-craq/
-mvn clean package
