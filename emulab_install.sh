cfaf5a1302f19348ef4219596370b8aee9917c3a
Jeffrey Han <itdelatrisu@gmail.com>
2017-05-10 22:22:42 -0400

    Change emulab environment

--- emulab_install.sh
+++ emulab_install.sh
@@ -4,15 +4,15 @@
 # Software installation on Ubuntu 14.04 LTS 64-bit
 # Emulab OSID: UBUNTU14-64-STD
 # ------------------------------------------------
+# sudo apt-get -y install software-properties-common python-software-properties
+# sudo add-apt-repository ppa:openjdk-r/ppa -y
+# sudo apt-get update
+# sudo apt-get -y install openjdk-8-jdk maven screen
+# sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 
-# install packages
-sudo apt-get -y install software-properties-common python-software-properties
-sudo add-apt-repository ppa:openjdk-r/ppa -y
+# ------------------------------------------------
+# Software installation on Ubuntu 16.04 LTS 64-bit
+# Emulab OSID: UBUNTU16-64-STD
+# ------------------------------------------------
 sudo apt-get update
-sudo apt-get -y install openjdk-8-jdk maven screen
-
-# configure java binaries
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-
-# print IP address
-/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'
+sudo apt-get -y install default-jdk maven screen
